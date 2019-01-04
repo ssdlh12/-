f9495b57214a2f74f4955c016040c3bb49bfb7c8
Liyc <1083247273@qq.com>
2019-01-04 09:36:55 +0800

    修改内容

--- git_test1/src/main/java/HelloGit.java
+++ git_test1/src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello word!");
         System.out.println("asfsdgdaf");
+        System.out.println("阿萨德干豆腐干豆腐");
     }
 }
