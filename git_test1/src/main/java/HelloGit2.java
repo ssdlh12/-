543dbf38351f0757a03a32b446a286e1cd4e496f
Liyc <1083247273@qq.com>
2019-04-15 15:11:58 +0800

    增加hellogit2

--- git_test1/src/main/java/HelloGit2.java
+++ git_test1/src/main/java/HelloGit2.java
@@ -1,6 +1,6 @@
 public class HelloGit2 {
     public static void main(String[] args) {
         System.out.println("阿萨德刚开金大福");
-        System.out.println("阿萨德刚卡戴珊");
+        System.out.println("阿萨德刚卡戴");
     }
 }
